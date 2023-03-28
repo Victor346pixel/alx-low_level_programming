69e459b0f1e4ebffcb0bdbd3fe5d5349a7a51680
victor346pixel <monicamwendia84@gmail.com>
2023-03-28 01:24:16 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rev_string - print a string in reverse
+ * @s: pointer pointing to the string
+ * Return: always 0
+*/
+
+void rev_string(char *s)
+{
+	int length, z, half;
+	char temp;
+
+	for (length = 0; s[length] != '\0'; length++)
+		;
+
+	z = 0;
+	half = length / 2;
+
+	while (half--)
+	{
+		temp = s[length - z - 1];
+		s[length - z - 1] = s[z];
+		s[z] = temp;
+		z++;
+	}
+}																																																																										
