85df47b3ae1145dbbee516708630c8450d52715d
victor346pixel <monicamwendia84@gmail.com>
2023-04-04 07:08:43 -0700

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -23,12 +23,12 @@ char *_strstr(char *haystack, char *needle)
 	if (haystack[index] == needle[index])
 	{
 		do {
-				if (needle[index + 1] == '\0')
+			if (needle[index + 1] == '\0')
 				return (haystack);
 
 				index++;
 
-				} while (haystack[index] == needle[index]);
+		} while (haystack[index] == needle[index]);
 	}
 	haystack++;
 	}
